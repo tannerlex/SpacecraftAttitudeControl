5800279b73f15b37048d9a28a08ab90a3d3adff1
Tanner Jones <tanner.jones@sdl.usu.edu>
2021-02-05 10:38:14 -0700

    added plotting as required for assignment 01 of ECE6345

--- B01 Orbital Environment/main.m
+++ B01 Orbital Environment/main.m
@@ -1,3 +1,9 @@
+%% Orbital Environment - Assignment 1
+% Name: Tanner Lex Jones
+
+%% Preliminaries
+
+% This cleans all variables and sets the format to display more digits.
 clearvars
 close all
 clc
@@ -34,9 +40,45 @@ A_IF = [cO*cw-cI*sO*sw, cw*sO+cO*cI*sw, sI*sw; ...
        -cO*sw-cI*cw*sO, cO*cI*cw-sO*sw, cw*sI; ...
                  sO*sI,         -cO*sI,    cI];
 A_FI = A_IF';
+
+%% The position and velocity vectors projected to the inertial frame are
+%% shown below.
 r0_I = A_FI*rF;
 v0_I = A_FI*vF;
+disp(r0_I);
+disp(v0_I);
 
 %% Simulate
 t_sim = 3600; % simulation time in seconds
-sim('orbit', t_sim)
+sim('orbit', t_sim);
+
+%% save the simulation output
+orbit = ans;
+
+%% plot of the position vector over the simulation period
+figure
+plot(orbit.r_I)
+title('r_I: Position Vector')
+figure
+plot(orbit.v_I)
+title('v_I: Velocity Vector')
+
+%% plot of the magnetic field vector
+figure
+plot(orbit.B_I)
+title('B_I: Magnetic Field Vector')
+
+%% plot of the sun vector
+figure
+plot(orbit.s_I)
+title('s_I: Sun Vector')
+
+%% plot of the eclipse state
+figure
+plot(orbit.sun)
+title('sun: Sun State (1 = not in eclipse)')
+
+%% plot of the atmospheric density
+figure
+plot(orbit.rho)
+title('rho: Atmospheric Density')
