755eac21cef8d4271a9a3224761ec3bd2ab2b77c
Tanner Jones <tanner.jones@sdl.usu.edu>
2021-02-10 16:37:15 -0700

    started disturbances assignment, gravity done (not tested)

---
+++ B02 Disturbance Torques/gravityTorque.m
@@ -0,0 +1,20 @@
+function Tg_B = gravityTorque(J_C_P, A_PB, uEarth, r_I, v_I, q_BI)
+
+% Declare the Orbital Reference frame
+zhat_IR = -r_I/norm(r_I);
+yhat_IR = cross(-r_I, v_I)/norm(cross(-r_I, v_I));
+xhat_IR = cross(yhat_IR, zhat_IR);
+A_RI = [xhat_IR'; yhat_IR'; zhat_IR'];
+A_IR = A_RI';
+A_BI = q2A(q_BI);
+A_PR = A_PB * A_BI * A_IR;
+
+% Calculate gravity torque vector using equation 3.157
+J1 = J_C_P(1,1);
+J2 = J_C_P(2,2);
+J3 = J_C_P(3,3);
+M = [(J3-J2) * A_PR(2,3) * A_PR(3,3);
+     (J1-J3) * A_PR(1,3) * A_PR(3,3);
+     (J2-J1) * A_PR(1,3) * A_PR(2,3)];
+Tg_P = 3*uEarth/norm(r_I)^3 * M;
+Tg_B = A_BP * Tg_P;
---
+++ B02 Disturbance Torques/main.m
@@ -0,0 +1,39 @@
+%% Disturbance Torques - Assignment 2
+% Name: Tanner Lex Jones
+
+%% Preliminaries
+
+% This cleans all variables and sets the format to display more digits.
+clearvars
+close all
+clc
+format long
+
+% Addpath to Attitude Representations Folder
+addpath('../01 Attitude Representations')
+
+% Addpath to Attitude Kinematics Folder
+addpath('../02 Attitude Kinematics')
+
+% Addpath to Attitude Dynamics Folder
+addpath('../03 Attitude Dynamics')
+
+% Load qBus
+load qBus.mat
+
+% Load Mass Properties
+mass_properties
+
+% Load dimensions
+dimensions
+
+%% Constants
+uEarth = 3.986004418e14; % Earth Gravitational Parameter (m^3/s^2) C.104a
+solarFlux = 1366; % Solar Flux (W/m^2)
+c = 2.9979e8;
+P_SR = solarFlux/c;
+
+%% Initial Conditions
+q0_BI.s = 1;
+q0_BI.v = [0;0;0];
+
