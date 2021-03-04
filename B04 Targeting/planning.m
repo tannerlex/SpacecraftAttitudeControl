f4e1153ba0c306f8b4ab409e7d00f8a817035790
Tanner Jones <tanner.jones@sdl.usu.edu>
2021-03-03 17:56:07 -0700

    got a qstar_PI timeseries

--- B04 Targeting/planning.m
+++ B04 Targeting/planning.m
@@ -22,6 +22,7 @@ t = angle_zenith.time; % seconds
 qs_BI = plan.qs_BI;
 ql_BI = plan.ql_BI;
 
+
 %% Find the angle command switch times
 
 % angle above horizon at which sat must be pointing at ground target
@@ -50,7 +51,7 @@ switch2gndtrkIdx = 0;
 for i = 1:(startGndTrkIdx - 1)
     if ((t(i) <= switch2gndtrk) && ...
         (t(i+1) > switch2gndtrk))
-        switch2gndtrkIdx = i
+        switch2gndtrkIdx = i;
     end
 end
 
@@ -61,54 +62,97 @@ i = stopGndTrkIdx;
 while 0 == resSunPointIdx
     i = i + 1;
     if (t(i) > resSunPoint)
-        resSunPointIdx = i
+        resSunPointIdx = i;
     end
 end
 
-%% Solve for the commanded quaternion throughout the mission.
+%% Solve for the commanded quaternions throughout the mission.
+% 1st period is sun tracking, use qs_PI
+% 2nd period is a slew between sun tracking and ground tracking
+% 3rd period is ground point tracking, use ql_PI
+% 4th period is a slew from the ground track to the sun
+% 5th period is sun tracking, use qs_PI
 
-% initialize variables used for pointing commands
+%% initialize variables used for pointing commands
+% qstar_PI gets initialized to qs_PI (covers 1st & 5th periods)
+% w and wdot get initialized to zero
 qstar_BI = qs_BI; % initialize to the sun pointing angle
+% qstar_PI = timeseries(q0_BI,t);
+qs_PI = qs_BI;
+for i = 1:length(t)
+    %convert sun pointing from Body to Principle
+    qsun_BI.s = qs_BI.s.data(i);
+    qsun_BI.v = qs_BI.v.data(i,:);
+    q = qUnit(A2q(A_PB*q2A(qsun_BI)));
+    qs_PI.v.data(i,:) = q.v;
+    qs_PI.s.data(i) = q.s;
+    %convert USU pointing from Body to Principle
+    qusu_BI.s = ql_BI.s.data(i);
+    qusu_BI.v = ql_BI.v.data(i,:);
+    q = qUnit(A2q(A_PB*q2A(qusu_BI)));
+    ql_PI.v.data(i,:) = q.v;
+    ql_PI.s.data(i) = q.s;
+end
+qstar_PI = qs_PI; % initialize to the sun pointing angle
 wbidotstar_P = timeseries(zeros(length(t),1),t);
 wbistar_P = timeseries(zeros(length(t),1),t);
 
-% determine slew command between sun and ground track
-q0_BIslew.v = qs_BI.v.data(switch2gndtrkIdx,:);
-q0_BIslew.s = qs_BI.s.data(switch2gndtrkIdx);
-qf_BIslew.v = ql_BI.v.data(startGndTrkIdx,:);
-qf_BIslew.s = ql_BI.s.data(startGndTrkIdx);
-[~, th_0] = q2e(q0_BIslew);
-[e, th_f] = q2e(qf_BIslew);
+%% 2nd period: determine slew command between sun and ground track
+q0_PIslew.v = qs_PI.v.data(switch2gndtrkIdx,:)';
+q0_PIslew.s = qs_PI.s.data(switch2gndtrkIdx);
+qf_PIslew.v = ql_PI.v.data(startGndTrkIdx,:)';
+qf_PIslew.s = ql_PI.s.data(startGndTrkIdx);
+q0_IPslew = qUnit(A2q(q2A(q0_PIslew)'));
+qe = qUnit(qX(qf_PIslew, q0_IPslew)); % error quaternion
+[ehat, th_f] = q2e(qe);
+th_0 = 0;
 for i = switch2gndtrkIdx:(startGndTrkIdx-1)
     [th, w, wdot] = ...
-        profiled_scalar(th_0, th_f, t(i), wdotmax, wmax);
-    q = e2q(e, th);
-    qstar_BI.s.data(i) = q.s;
-    qstar_BI.v.data(i,:) = q.v;
+        profiled_scalar(th_0, th_f, t(i)-switch2gndtrk, wdotmax, wmax);
+    qr = qUnit(e2q(ehat, th));
+    qs = qUnit(qX(qr, q0_PIslew));
+    qstar_PI.s.data(i) = qs.s;
+    qstar_PI.v.data(i,:) = qs.v;
     wbistar_P.data(i) = w;
     wbidotstar_P.data(i) = wdot;
 end
 
-% track ground track
+%% 3rd period: track ground track
 for i = startGndTrkIdx:stopGndTrkIdx
-    qstar_BI.v.data(i,:) = ql_BI.v.data(i,:);
-    qstar_BI.s.data(i) = ql_BI.s.data(i);
+    qstar_PI.v.data(i,:) = ql_PI.v.data(i,:);
+    qstar_PI.s.data(i) = ql_PI.s.data(i);
 end
 
-% determine slew command between ground track and sun
-q0_BIslew.v = ql_BI.v.data(stopGndTrkIdx,:);
-q0_BIslew.s = ql_BI.s.data(stopGndTrkIdx);
-qf_BIslew.v = ql_BI.v.data(resSunPointIdx,:);
-qf_BIslew.s = ql_BI.s.data(resSunPointIdx);
-[~, th_0] = q2e(q0_BIslew);
-[e, th_f] = q2e(qf_BIslew);
+%% 4th period: determine slew command between ground track and sun
+q0_PIslew.v = ql_PI.v.data(stopGndTrkIdx,:)';
+q0_PIslew.s = ql_PI.s.data(stopGndTrkIdx);
+qf_PIslew.v = qs_PI.v.data(resSunPointIdx,:)';
+qf_PIslew.s = qs_PI.s.data(resSunPointIdx);
+q0_IPslew = qUnit(A2q(q2A(q0_PIslew)'));
+qe = qUnit(qX(qf_PIslew, q0_IPslew)); % error quaternion
+[ehat, th_f] = q2e(qe);
+th_0 = 0;
 for i = stopGndTrkIdx:(resSunPointIdx-1)
     [th, w, wdot] = ...
-        profiled_scalar(th_0, th_f, t(i), wdotmax, wmax);
-    qstar_BI.s.data(i) = q.s;
-    qstar_BI.v.data(i,:) = q.v;
+        profiled_scalar(th_0, th_f, t(i)-stopGndTrk, wdotmax, wmax);
+    qr = qUnit(e2q(ehat, th));
+    qs = qUnit(qX(qr, q0_PIslew));
+    qstar_PI.s.data(i) = qs.s;
+    qstar_PI.v.data(i,:) = qs.v;
     wbistar_P.data(i) = w;
     wbidotstar_P.data(i) = wdot;
 end
 
-% sim('animation', t_sim)
+
+%% Convert from principle frame to body frame for animation
+for i = 1:length(t)
+    q_PI.s = qstar_PI.s.data(i);
+    q_PI.v = qstar_PI.v.data(i,:);
+    q = qUnit(A2q(A_BP*q2A(q_PI)));
+    qstar_BI.v.data(i,:) = q.v;
+    qstar_BI.s.data(i) = q.s;
+end
+plot(qstar_BI.v)
+hold on
+plot(qstar_BI.s)
+sim('animation', t_sim)
